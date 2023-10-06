f90eefc96cf839f2c0d027e6b45c6183c36ac90c
abdul <abdulsk054@gmail.com>
2023-10-06 09:04:49 +0530

    changed

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_ssm_parameter" "main" {
   for_each = var.parameters
-  name  = each.key
-  type  = each.value["type"]
-  value = each.value["value"]
+  name   = each.key
+  type   = each.value["type"]
+  value  = each.value["value"]
+  key_id = "arn:aws:kms:us-east-1:219961291665:key/e83bb4c5-1a13-41b7-b6d3-8acda3747e9f"
 }
 
